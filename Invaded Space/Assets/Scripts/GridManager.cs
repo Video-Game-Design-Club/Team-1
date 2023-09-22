327e3fbfc147e6c461eab253b8a56c393abc406e
Noodlemanifolf <roxannecampbell@csus.edu>
2023-09-22 05:12:57 -0700

    Basic Interactions
    Added support for mouse events. Right now I just have it doing simple square highlighting but we should be able to add more quite easily

--- Invaded Space/Assets/Scripts/GridManager.cs
+++ Invaded Space/Assets/Scripts/GridManager.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+/**
+This class is responsible for handling all the grid square sprites, click events on the grid,
+    and keeping track of all objects currently placed on the grid
+**/
 public class GridManager : MonoBehaviour
 {
     [SerializeField]
@@ -25,6 +29,8 @@ public class GridManager : MonoBehaviour
 
     GameObject[,] gridObjects;
     SpriteRenderer[,] gridSprites;
+
+    SpriteRenderer highlightedSquare;
 
     void Awake(){
         UpdateGridSize();
@@ -68,7 +74,9 @@ public class GridManager : MonoBehaviour
     }
 
 
-    //I feel like this one explains itself well
+    /**
+    Update background sprite & collider for when the grid size changes
+    **/
     public void UpdateGridSize(){
         //these are here so this works in editor
         backgroundSprite = GetComponent<SpriteRenderer>();
@@ -78,10 +86,44 @@ public class GridManager : MonoBehaviour
         backgroundSprite.size = (Vector2)gridSize * gridSquareSize;
         boxCollider.size = scale;
 
-        Inititialize();
+        Inititialize();//update everything else
+    }
+
+    /**
+    Handle events from the MouseCast class on the main camera
+    We can do whatever we want here, I just have basic highlighting right now
+    **/
+    public void MouseHover(bool hit, Vector3 pos){
+        if (highlightedSquare != null){
+            highlightedSquare.color = squareColor;
+            highlightedSquare = null;
+        }
+
+        if(hit){
+            Vector3 localPos = transform.InverseTransformPoint(pos);
+            Vector2Int coords = LocalPosToSquare(localPos);
+            highlightedSquare = gridSprites[coords.x,coords.y];
+            highlightedSquare.color = squareColorHighlight;
+        }
+    }
+
+    /**
+    Converts a position in local space to a grid square coordinate
+    Right now this is used for figuring out what square the mouse cursor is over
+    **/
+    Vector2Int LocalPosToSquare(Vector3 pos){
+        pos = new Vector3(pos.x,pos.y,0f);
+        pos /= gridSquareSize;
+        pos += new Vector3(gridSize.x*0.5f,gridSize.y*0.5f,0f);
+        //clamping prevents weird & rare bugs where we might get a coord of -1 for some reason
+        int x = Mathf.Clamp(Mathf.FloorToInt(pos.x),0,gridSize.x);
+        int y = Mathf.Clamp(Mathf.FloorToInt(pos.y),0,gridSize.y);
+        Vector2Int res = new Vector2Int(x,y);
+        return res;
     }
 }
 
+//Boilerplate code for getting the fancy update button in the inspector :>
 [CustomEditor(typeof(GridManager))]
 public class InspectorGridManager : Editor
 {
---
+++ Invaded Space/Assets/Scripts/MouseCast.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+This is just a simple class to do a raycast from the mouse cursor into the world,
+    and letting the grid system know what it finds
+
+(I've had super bad experiences using Unity UI for things it was not specifically designed
+    for, which is why I'm making my own mouse click system for this. I think we should
+    be OK using Unity UI stuff for the shop and menus and all that)
+**/
+public class MouseCast : MonoBehaviour
+{
+
+    Camera cam;
+    [SerializeField]
+    LayerMask mask;
+    [SerializeField]
+    GridManager grid;
+
+    int gridLayer;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+        gridLayer = LayerMask.NameToLayer("Grid");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hi;
+        if (ScreenCast(mask, out hi) && hi.collider.gameObject.layer == gridLayer) {
+            grid.MouseHover(true,hi.point);
+        }else{
+            grid.MouseHover(false,Vector3.zero);
+        }
+    }
+
+    private bool ScreenCast(LayerMask mask, out RaycastHit hit) { //casts a ray from the camera, through the mouse cursor, into the world
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        return Physics.Raycast(ray, out hit, cam.farClipPlane, mask);
+    }
+
+    private Vector3 GetMouseWorldPosition(Vector2 position) { //converts screen space to world space
+        Vector3 pos3D = new Vector3(position.x, position.y, 5.0f);
+        return cam.ScreenToWorldPoint(pos3D);
+    }
+}
